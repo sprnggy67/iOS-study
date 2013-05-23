640eba85d5a4b119e0d7aecb64d54c701851851f
Dave Springgay <sprnggy67@gmail.com>
2013-05-23 13:17:50 +0100

    Hack vertical and horizontal scrolling to work

--- BigFeedReader/FeedReader/Supporting Files/Templates/src/renderRuntime.js
+++ BigFeedReader/FeedReader/Supporting Files/Templates/src/renderRuntime.js
@@ -4,12 +4,19 @@ console.info("Loaded renderRuntime.js");
 
 function loaded() {
 	$(".link").click(tapToNavigate);
-	$(".scroll").each(function() {
-		myScroll = new iScroll(this,{ hideScrollbar:true });
-	});
+    $(".scroll").each(function() {
+        myScroll = new iScroll(this,{
+            hideScrollbar:true,
+            hscroll:false,
+            onBeforeScrollStart: function ( e ) {
+                if ( this.absDistY > (this.absDistX + 5 ) ) {
+                    // user is scrolling the y axis, so prevent the browsers' native scrolling
+                    e.preventDefault();
+                }
+            }
+        });
+    });
 }
-
-// document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
 
 document.addEventListener('DOMContentLoaded', function () { setTimeout(loaded, 200); }, false);
 
